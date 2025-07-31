c808c84a868599698239bc8c840263c733fe541a
danapalania <dana.p.alania@ciit.edu.ph>
2025-07-31 23:46:19 +0800

    chat

--- FriendFinder/src/app/(home)/(tabs)/chats.tsx
+++ FriendFinder/src/app/(home)/(tabs)/chats.tsx
@@ -1,6 +1,10 @@
+import { use, useState } from 'react';
 import { Text } from 'react-native';
-
+import { ChannelList } from 'stream-chat-expo';
+import { Channel as ChannelType, StreamChat} from 'stream-chat';
+import { router } from 'expo-router';
 
 export default function MainTab() {
-    return <Text>Chats</Text>
+
+    return <ChannelList onSelect={(channel => router.push(`/channel/${channel.cid}`))}/>
 }
--- FriendFinder/src/app/(home)/_layout.tsx
+++ FriendFinder/src/app/(home)/_layout.tsx
@@ -1,5 +1,42 @@
 import { Slot, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, OverlayProvider } from 'stream-chat-expo';
+
+const client = StreamChat.getInstance('ehp59nmz9q48');
 
 export default function HomeLayout() {
-    return <Slot/>;
+
+    useEffect(() => {
+    const connect = async () => {
+        // Only connect if a user is not already connected
+        if (!client.user) {
+        await client.connectUser(
+            {
+            id: 'test',
+            name: 'John Doe',
+            image: 'https://i.imgur.com/fR9Jz14.png',
+            },
+            client.devToken('test')
+        );
+        // commented out channel to avoid duplicate creation
+        //const channel = client.channel('messaging', 'test', {
+        //    name: 'Test',
+        //});
+        //await channel.watch();
+        }
+    };
+
+    connect();
+    }, []);
+
+    return (
+        <OverlayProvider>
+            <Chat client={client}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+                </Stack>
+            </Chat>
+        </OverlayProvider>
+    );
 }
